c24c08efbdecd2c375565312a28029cb498945e0
Djamal dos Santos <leondark.tzk@gmail.com>
2020-04-19 12:26:35 +0200

    api#4 Update Dependencies and Config variables

--- api/app/config.js
+++ api/app/config.js
@@ -5,6 +5,7 @@ const config = {}
 //const bodyParse = {}
 //const hemlet = {}
 const servue = {}
+const parser = {}
 
 if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
     dotenv.config({path: '.env'})
@@ -36,11 +37,18 @@ config.TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID
 config.TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
 config.TWILIO_NUMBER = process.env.TWILIO_NUMBER
 
+// Parser Variables config
+parser.APP_ID = process.env.PARSE_APP_ID
+parser.JS_KEY = process.env.PARSE_JS_KEY
+parser.SERVER_URL = process.env.PARSE_SERVER_URL
+
+config.PARSER = parser
+
 const requiredConfig = [
     config.LOGGER,
     config.COVID19MZ,
-    config.TWILIO_ACCOUNT_SID, 
-    config.TWILIO_AUTH_TOKEN, 
+    config.TWILIO_ACCOUNT_SID,
+    config.TWILIO_AUTH_TOKEN,
     config.TWILIO_NUMBER
 ]
 
@@ -50,7 +58,7 @@ var isConfigured = requiredConfig.every(function(value) {
 
 if (!isConfigured) {
     const errorMessage = 'LOGGER, COVID19MZ, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_NUMBER must be set.'
-    throw new Error(errorMessage) 
+    throw new Error(errorMessage)
 }
 
 module.exports = config
