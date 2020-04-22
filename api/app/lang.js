5752d6eba66c2df255159271bb1a5792aa025bbd
Djamal dos Santos <leondark.tzk@gmail.com>
2020-04-22 19:40:22 +0200

    api#10 Update Dependencies & lang.js and Helpers

--- api/app/lang.js
+++ api/app/lang.js
@@ -12,18 +12,20 @@ eng.ALREADY_SUBSCRIBED = `The number is registered on the platform.`
 eng.NOT_SUBSCRIBED = `The number is not registered on the platform.`
 eng.SUBSCRIBED_SUCESS = `was subscribed successfully.`
 eng.SUBSCRIBED_FAILURE = `was not subscribed. Is not a Mozambican valid number.`
+pt.UNSUBSCRIBED_SUCESS = `was unsubscribed successfully.`
 
 // PT DEFINED VARIABLES
-eng.ERROR_REQUEST_FAILED = `Falha na Solicitação ao Servidor.`
-eng.ERROR_NOT_FOUND = `Endereço não encontrado.`
+pt.ERROR_REQUEST_FAILED = `Falha na Solicitação ao Servidor.`
+pt.ERROR_NOT_FOUND = `Endereço não encontrado.`
 
-eng.SERVER_ALIVE = `O servidor está activo.`
+pt.SERVER_ALIVE = `O servidor está activo.`
 pt.ALREADY_SUBSCRIBED = `O Número já está registado na plataforma.`
 pt.NOT_SUBSCRIBED = `O Número não está registado na plataforma.`
-pt.SUBSCRIBED_SUCESS = `O Número foi registado com sucesso.`
-pt.SUBSCRIBED_FAILURE = `O Número não foi registado. Não é um número válido em Moçambique.`
+pt.SUBSCRIBED_SUCESS = `foi subscrito com sucesso.`
+pt.SUBSCRIBED_FAILURE = `não foi registado. Não é um número válido em Moçambique.`
+pt.UNSUBSCRIBED_SUCESS = `foi removida a subscrição com sucesso.`
 
 lang.ENG = eng
-lang.PT = PT
+lang.PT = pt
 
 module.exports = lang
