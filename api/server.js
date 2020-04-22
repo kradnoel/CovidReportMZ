3374b8eef5607f669f332b1c75ac25b54077738b
Djamal dos Santos <leondark.tzk@gmail.com>
2020-04-22 21:47:03 +0200

    api#13 Config Cron Task (SendSmsToSubscribedUsers)

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 // Require dependencies
 const config = require('./app/config')
 const routes = require('./app/routes')
+const {SendSmsToSubscribedUsers} = require('./app/parse')
 const logger = require('morgan')
 const compression = require('compression')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
 const express = require('express')
+const cron = require('node-cron')
 
 // Create an Express application
 const app = express()
@@ -20,5 +22,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(helmet())
 app.use(routes)
+
+// Send sms to subscribed users daily
+cron.schedule('00 08 * * *', () => {
+	SendSmsToSubscribedUsers()
+},{
+	"schedule": true,
+	"timezone": "Africa/Harare"
+})
 
 module.exports = app
